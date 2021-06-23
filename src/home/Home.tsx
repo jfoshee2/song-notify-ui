f566ee505d35e21fe10ae62831611378eed8e29f
Foshee <jfoshee3@elsys.gtri.org>
2021-06-23 17:21:55 -0400

    Adds Songs Table

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,1 +1,29 @@
-﻿// TODO: implement Home component
+﻿import { AppBar, Tabs, Tab } from '@material-ui/core';
+import React, { useState } from 'react';
+import SongTable from '../table/SongTable';
+
+const Home = (): React.ReactElement => {
+
+    const [currentTab, setCurrentTab] = useState(0);
+
+    const handleTabChange = (event: any, newValue: number) => {
+        setCurrentTab(newValue);
+    };
+
+    return(
+        <div>
+            <AppBar position="static">
+                <Tabs value={currentTab} onChange={handleTabChange}>
+                    <Tab label="Songs" />
+                    <Tab label="Stations" />
+                    <Tab label="Daily Schedule" />
+                </Tabs>
+            </AppBar>
+            {currentTab === 0 && <SongTable />}
+            {currentTab === 1 && <div>Stations</div>}
+            {currentTab === 2 && <div>DailySchedule</div>}
+        </div>
+    );
+}
+
+export default Home;
--- src/table/SongTable.tsx
+++ src/table/SongTable.tsx
@@ -1,1 +1,44 @@
-﻿// TODO: implement SongTable component
+﻿import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableHead, TableRow, TableCell } from "@material-ui/core";
+import axios from 'axios';
+
+
+const SongTable = (): React.ReactElement => {
+
+    type SongType = {
+        id: number,
+        name: string,
+        genre: string
+    };
+
+    const [songList, setSongList] = useState([] as SongType[]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/song/").then(res => {
+            setSongList(res.data);
+        });
+    }, []);
+
+    return(
+        <div>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell style={{ fontWeight: "bold" }}>Name</TableCell>
+                        <TableCell style={{ fontWeight: "bold" }}>Genre</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {songList.map((song) => (
+                        <TableRow key={song.id}>
+                            <TableCell>{song.name}</TableCell>
+                            <TableCell>{song.genre}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
+
+export default SongTable;
